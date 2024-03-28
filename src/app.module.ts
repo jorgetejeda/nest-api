53b4e5024bfe23f21d7a716f299c673be143f765
Jorge Tejeda <jorgetejeda0804@gmail.com>
2024-03-28 17:22:20 -0400

    :sparkles: add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CatsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env.development' }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
